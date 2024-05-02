4ea672b4b6de0d9b8d1cd5418dd67bf13ab17fc8
ACM1 <zahimon@hotmail.com>
2024-05-02 17:37:12 +0200

    Tercer_cambio

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones con Git");
 		System.out.println("Este es otro text");
+		System.out.println("Este es otro text 2");
 	}
 }
